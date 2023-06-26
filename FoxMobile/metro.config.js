e350ee9c35b7bb4d975af10d181c301533851125
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-06-26 17:46:32 -0400

    removed node modules

---
+++ FoxMobile/metro.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    resolver: {
+      assetExts: ['jsx', 'js', 'json', 'png', 'jpg', 'jpeg', 'gif', 'svg'], // Add any other necessary file extensions for your project
+    },
+  };
+  
