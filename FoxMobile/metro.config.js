03d958a7123639d79d2953f852d878dca0501349
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-06-26 17:50:26 -0400

    starting from scratch

--- FoxMobile/metro.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    resolver: {
-      assetExts: ['jsx', 'js', 'json', 'png', 'jpg', 'jpeg', 'gif', 'svg'], // Add any other necessary file extensions for your project
-    },
-  };
-  
