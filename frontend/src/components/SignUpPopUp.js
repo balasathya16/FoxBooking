be14621a3983bddb9f530c34f01b20853fd1513d
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-07-01 23:12:17 -0400

    sign up with css

--- frontend/src/components/SignUpPopUp.js
+++ frontend/src/components/SignUpPopUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaGoogle, FaFacebook, FaEnvelope } from 'react-icons/fa';
 import '../styles/SignUpPopUp.css'; // Import the CSS file
 
 const SignUpPopup = ({ onClose }) => {
@@ -18,12 +19,15 @@ const SignUpPopup = ({ onClose }) => {
     <div className="signup-popup">
       <div className="popup-content">
         <button className="mb-4" onClick={handleSignUpWithGoogle}>
+          <FaGoogle className="icon" />
           Sign up with Google
         </button>
         <button className="mb-4" onClick={handleSignUpWithFacebook}>
+          <FaFacebook className="icon" />
           Sign up with Facebook
         </button>
         <button className="mb-4" onClick={handleSignUpWithEmail}>
+          <FaEnvelope className="icon" />
           Sign up with Email
         </button>
         <button onClick={onClose}>Close</button>
