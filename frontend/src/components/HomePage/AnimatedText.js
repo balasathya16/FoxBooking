891352b2a2710255e5114b73efe9e29f8849beab
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-07-03 16:23:15 -0400

    added files

---
+++ frontend/src/components/HomePage/AnimatedText.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../styles/AnimatedText.css';
+
+const AnimatedText = () => {
+  return (
+    <div className="animated-text-container">
+      <div className="background-image" />
+      <h1 className="animated-text">
+        Step up your game. Book a sports arena and conquer.
+      </h1>
+    </div>
+  );
+};
+
+export default AnimatedText;
