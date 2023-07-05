06a9768463b63e6b968195f92dd0112a3df313f6
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-07-05 15:05:01 -0400

    creating tiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
-import AnimatedText from './components/HomePage/AnimatedText';
 import Footer from './components/Footer';
 import HomePage from './components/HomePage/HomePage';
 import './styles/Header.css';
-import './styles/AnimatedText.css';
 import './styles/Footer.css';
 import './styles/tailwind.css';
-import './styles/GalleryContainer.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <div className="App-content">
-        <AnimatedText />
-        <HomePage /> 
+        <HomePage />
       </div>
       <Footer />
     </div>
--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import GalleryContainer from './GalleryContainer';
+import AnimatedText from './AnimatedText';
+import GalleryTile from './GalleryTile';
+import '../../styles/HomePage.css';
 
 const HomePage = () => {
-  console.log('Rendering HomePage'); // Add this line
   return (
     <div className="home-page">
-      <h2>This is the Home Page</h2>
-      <p>Some text to check if the Home Page component renders properly.</p>
-      <GalleryContainer />
-      <GalleryContainer />
-      <GalleryContainer />
+      <AnimatedText />
+
+      <div className="gallery">
+        <div className="gallery-container">
+          <GalleryTile name="Cricket" />
+        </div>
+        <div className="gallery-container">
+          <GalleryTile name="Football" />
+        </div>
+        <div className="gallery-container">
+          <GalleryTile name="Badminton" />
+        </div>
+      </div>
+
+      {/* Add your other content */}
     </div>
   );
 };
-
 
 export default HomePage;
