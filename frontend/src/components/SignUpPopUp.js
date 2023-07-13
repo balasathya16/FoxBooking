a9f9926ceb5dd10d94d08760842412ab33c3771f
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-07-13 16:05:59 -0400

    removed phone sign up component

--- frontend/src/components/SignUpPopUp.js
+++ frontend/src/components/SignUpPopUp.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-import handleGoogleSignUp from './GoogleSignUp'; // Import the handleGoogleSignUp function
-import handleEmailSignUp from './EmailSignUp'; // Import the handleEmailSignUp function
+import handleGoogleSignUp from './GoogleSignUp';
+import handleEmailSignUp from './EmailSignUp';
 import '../styles/SignUpPopUp.css';
 
 const SignUpPopUp = ({ onClose }) => {
@@ -14,13 +15,10 @@ const SignUpPopUp = ({ onClose }) => {
     handleEmailSignUp(email, password);
   };
 
-  const handlePhoneSignUp = () => {
-    // Implement phone sign up functionality
-    // ...
-  };
+
 
   return (
-    <div className="signup-popup">
+    <div id="signup-popup" className="signup-popup">
       <div className="popup-content">
         <h3>Sign up</h3>
         <button className="popup-button" onClick={handleGoogleSignUp}>
@@ -30,10 +28,6 @@ const SignUpPopUp = ({ onClose }) => {
         <button className="popup-button" onClick={handleEmailSignUpClick}>
           <FontAwesomeIcon icon={faEnvelope} className="icon" />
           Sign up with Email
-        </button>
-        <button className="popup-button" onClick={handlePhoneSignUp}>
-          <FontAwesomeIcon icon={faPhone} className="icon" />
-          Sign up with phone number
         </button>
         <button className="close-button" onClick={onClose}>
           Close
