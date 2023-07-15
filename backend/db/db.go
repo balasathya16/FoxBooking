b7c48a277185649e9a2bd0017739c812ba56a7be
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-07-15 16:52:25 -0400

    error logging - mongo db

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
 
 	"github.com/balasathya16/FoxBooking/models"
@@ -12,6 +14,9 @@ import (
 func ConnectDB() (*mongo.Database, error) {
 	// Get the MongoDB connection string from the environment variable
 	connectionString := os.Getenv("DB_CONNECTION_STRING")
+	if connectionString == "" {
+		return nil, errors.New("DB_CONNECTION_STRING environment variable not set")
+	}
 
 	// Set up the MongoDB connection options
 	clientOptions := options.Client().ApplyURI(connectionString)
@@ -19,13 +24,13 @@ func ConnectDB() (*mongo.Database, error) {
 	// Connect to the MongoDB database
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to connect to MongoDB: %v", err)
 	}
 
 	// Check the connection
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to ping MongoDB: %v", err)
 	}
 
 	// Get a handle to the database
