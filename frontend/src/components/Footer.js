da92533e9aae9571b0f4ea6aef092f5523152e97
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-07-16 19:37:24 -0400

    footer clean up

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   const [showFooter, setShowFooter] = useState(false);
@@ -11,22 +13,40 @@ const Footer = () => {
       setShowFooter(isScrollAtBottom);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    // Add debouncing to the scroll event listener
+    let timeoutId;
+    const debouncedScroll = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(handleScroll, 100); // Adjust the delay (in milliseconds) as needed
+    };
+
+    window.addEventListener('scroll', debouncedScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', debouncedScroll);
     };
   }, []); // Empty dependency array to ensure the effect is executed only once
 
-  if (!showFooter) {
-    return null;
-  }
-
   return (
-    <footer className="footer">
+    <footer className={`footer ${showFooter ? 'visible' : ''}`}>
       <div className="container">
-        <div className="footer-bottom">
-          <p>&copy; 2023 FoxBooking. <a href="/privacy">Privacy</a> <a href="/terms">Terms</a></p>
+        <div className="footer-content">
+          <div className="footer-left">
+            <p>&copy; 2023 FoxBooking <a href="/privacy">Privacy</a> <a href="/terms">Terms</a></p>
+          </div>
+          <div className="footer-right">
+            <div className="social-icons">
+              <a href="https://www.youtube.com/your-youtube-page" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faYoutube} className="social-icon" />
+              </a>
+              <a href="https://www.instagram.com/your-instagram-page" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} className="social-icon" />
+              </a>
+              <a href="https://www.linkedin.com/in/your-linkedin-page" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
