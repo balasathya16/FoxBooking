6b89f4a92e3ffc39a860f9f2412b410796e23280
balasathya16 <balasathya16@yahoo.com>
2023-07-28 19:24:45 -0400

    stylized the privacy page

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -1,7 +1,10 @@
+// src/routes/AppRouter.js
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../components/HomePage/HomePage';
 import ListingDetailsPage from '../pages/ListingDetailsPage';
+import PrivacyPolicy from '../pages/PrivacyPolicy'; // Import the PrivacyPolicy component
 
 const AppRouter = () => {
   return (
@@ -9,6 +12,8 @@ const AppRouter = () => {
       {/* Define your routes here */}
       <Route path="/" element={<HomePage />} />
       <Route path="/listing/:id" element={<ListingDetailsPage />} />
+      {/* Add the route for the Privacy Policy page */}
+      <Route path="/privacy" element={<PrivacyPolicy />} />
     </Routes>
   );
 };
