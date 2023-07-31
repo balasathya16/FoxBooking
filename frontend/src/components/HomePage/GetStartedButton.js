2268cecefd102925ae836f5774dd28572a5ef4ce
balasathya16 <balasathya16@yahoo.com>
2023-07-31 01:06:05 -0400

    changed sign up text

--- frontend/src/components/HomePage/GetStartedButton.js
+++ frontend/src/components/HomePage/GetStartedButton.js
@@ -4,7 +4,7 @@ const GetStartedButton = ({ onClick }) => {
   return (
     <div className="get-started-container">
       <button className="get-started-button" onClick={onClick}>
-        Sign up NOW!
+        Sign up
       </button>
     </div>
   );
