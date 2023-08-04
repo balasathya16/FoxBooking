5c7d80eab3aaa81ba9b21d3aefe38b7f9ee90296
balasathya16 <balasathya16@yahoo.com>
2023-08-04 17:04:29 -0400

    added the files

---
+++ frontend/src/pages/AvailabilityCalendar.js
@@ -0,0 +1,35 @@
+// src/pages/AvailabilityCalendar.js
+
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const AvailabilityCalendar = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    // Add your logic here to handle the selected date and time
+  };
+
+  // Log any errors that might occur in the component
+  if (selectedDate instanceof Date === false) {
+    console.error('Error: selectedDate is not a valid Date object.');
+  }
+
+  return (
+    <div>
+      {/* Render the availability calendar component here */}
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleDateChange}
+        minDate={new Date()} // Optionally set a minimum date
+        // You can add more props to customize the appearance and behavior of the picker
+      />
+      {/* You can display the selected date and time to the user */}
+      <p>Selected Date: {selectedDate.toString()}</p>
+    </div>
+  );
+};
+
+export default AvailabilityCalendar;
---
+++ frontend/src/pages/PrivacyPolicy.js
@@ -0,0 +1,79 @@
+// PrivacyPolicy.js
+
+import '../styles/PrivacyPolicy.css'; // Import the PrivacyPolicy.css file
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  // Add the Privacy Policy content here (the content you provided earlier)
+  const privacyContent = `
+Thank you for visiting our website ("Website"). This Privacy Policy is designed to help you understand how we collect, use, and protect your personal information when you use our services to book indoor and outdoor cricket and sporting courts. Please take a moment to read this Privacy Policy carefully to ensure that you are aware of our practices and your rights concerning your personal information.
+
+**1. Information We Collect**
+
+a. **Personal Information:** When you use our Website to book cricket and sporting courts, we may collect certain personal information from you. This may include but is not limited to your name, contact details (email address and phone number), and billing information.
+
+b. **Booking Information:** To facilitate court bookings, we may collect details about the type of court you are interested in, the date and time of your booking, and any additional preferences you provide.
+
+c. **User Account Information:** If you choose to create an account on our Website, we may collect and store your username, password, and other account-related information.
+
+d. **Device and Usage Information:** We may automatically collect certain information about the device you use to access our Website, such as your IP address, browser type, operating system, and browsing behavior.
+
+e. **Cookies and Similar Technologies:** We may use cookies and similar tracking technologies to enhance your experience on our Website, remember your preferences, and analyze how you interact with our services.
+
+**2. How We Use Your Information**
+
+a. **Court Bookings:** We use the information you provide to facilitate and manage your court bookings effectively.
+
+b. **Communication:** We may use your contact information to communicate with you about your bookings, provide updates about our services, and respond to your inquiries.
+
+c. **Improvement of Services:** Your feedback and usage patterns may be used to improve our services, website functionality, and user experience.
+
+d. **Marketing:** With your consent, we may send you promotional emails or messages about our services, special offers, and events. You have the option to unsubscribe from marketing communications at any time.
+
+e. **Legal Compliance:** We may use your information to comply with applicable laws, regulations, and legal processes.
+
+**3. Data Security**
+
+We are committed to safeguarding your personal information and have implemented reasonable measures to protect it from unauthorized access, use, or disclosure. However, please note that no data transmission over the internet or electronic storage method can be guaranteed to be 100% secure. We encourage you to maintain the confidentiality of your account credentials and report any unauthorized access to us immediately.
+
+**4. Data Sharing**
+
+a. **Service Providers:** We may share your personal information with third-party service providers who assist us in operating our Website and providing services to you, such as payment processors, hosting providers, and customer support services.
+
+b. **Legal Requirements:** We may disclose your personal information to comply with applicable laws, regulations, legal processes, or governmental requests.
+
+c. **Business Transfers:** In the event of a merger, acquisition, or sale of all or a portion of our assets, your personal information may be transferred to the acquiring entity.
+
+**5. Your Rights**
+
+a. **Access and Correction:** You have the right to access and update the personal information we hold about you. You can do this by logging into your account or contacting us directly.
+
+b. **Withdraw Consent:** If you have given us consent to process your personal information for specific purposes, you have the right to withdraw that consent at any time.
+
+c. **Data Deletion:** You may request the deletion of your personal information, and we will comply with such requests, subject to legal obligations and legitimate business purposes.
+
+d. **Data Portability:** You have the right to request a copy of your personal information in a machine-readable format.
+
+**6. Children's Privacy**
+
+Our Website is not intended for use by individuals under the age of 18. We do not knowingly collect personal information from children, and if we become aware that a child has provided us with their personal information without parental consent, we will take steps to remove the information from our records.
+
+**7. Changes to this Privacy Policy**
+
+We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements. We will notify you of any material changes by posting the updated Privacy Policy on our Website and updating the "Effective Date" at the top of the policy.
+
+**8. Contact Us**
+
+If you have any questions, concerns, or requests regarding this Privacy Policy or the processing of your personal information, please contact us at [contact email/phone number].
+
+By using our Website, you agree to the terms and conditions outlined in this Privacy Policy.`.trim();
+
+  return (
+    <div className="privacy-container">
+      <h1>Privacy Policy</h1>
+      <p>{privacyContent}</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
