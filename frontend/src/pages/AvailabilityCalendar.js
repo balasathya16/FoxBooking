4401f130ccd03a87fd08384a9a10dde57caeecfd
balasathya16 <balasathya16@yahoo.com>
2023-08-05 13:55:46 -0400

    added styling to calendar

--- frontend/src/pages/AvailabilityCalendar.js
+++ frontend/src/pages/AvailabilityCalendar.js
@@ -1,34 +1,68 @@
-// src/pages/AvailabilityCalendar.js
-
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import DateTimePicker from 'react-datetime-picker';
 import 'react-datepicker/dist/react-datepicker.css';
-import '../styles/AvailabilityCalendar.css'
+import '../styles/AvailabilityCalendar.css';
 
 const AvailabilityCalendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
-    // Add your logic here to handle the selected date and time
   };
 
-  // Log any errors that might occur in the component
-  if (selectedDate instanceof Date === false) {
-    console.error('Error: selectedDate is not a valid Date object.');
-  }
+  const handleStartTimeChange = (time) => {
+    setStartTime(time);
+  };
+
+  const handleEndTimeChange = (time) => {
+    setEndTime(time);
+  };
+
+  // Calculate the time duration in hours
+  const timeDuration = (endTime.getTime() - startTime.getTime()) / (1000 * 60 * 60);
 
   return (
     <div>
-      {/* Render the availability calendar component here */}
-      <DatePicker
-        selected={selectedDate}
-        onChange={handleDateChange}
-        minDate={new Date()} // Optionally set a minimum date
-        // You can add more props to customize the appearance and behavior of the picker
-      />
-      {/* You can display the selected date and time to the user */}
-      <p>Selected Date: {selectedDate.toString()}</p>
+      {/* Render the availability calendar and time picker components here */}
+      <div>
+        <h2>Select Date</h2>
+        <DateTimePicker
+          onChange={handleDateChange}
+          value={selectedDate}
+          minDate={new Date()} // Optionally set a minimum date
+          // You can add more props to customize the appearance and behavior of the date picker
+        />
+        <p>Selected Date: {selectedDate.toString()}</p>
+      </div>
+
+      <div>
+        <h2>Select Start Time</h2>
+        <DateTimePicker
+          onChange={handleStartTimeChange}
+          value={startTime}
+          disableClock={true} // Disable the clock for time picking
+          // You can add more props to customize the appearance and behavior of the time picker
+        />
+        <p>Selected Start Time: {startTime.toString()}</p>
+      </div>
+
+      <div>
+        <h2>Select End Time</h2>
+        <DateTimePicker
+          onChange={handleEndTimeChange}
+          value={endTime}
+          disableClock={true} // Disable the clock for time picking
+          // You can add more props to customize the appearance and behavior of the time picker
+        />
+        <p>Selected End Time: {endTime.toString()}</p>
+      </div>
+
+      <div>
+        <h2>Time Duration</h2>
+        <p>{timeDuration.toFixed(2)} hours</p>
+      </div>
     </div>
   );
 };
