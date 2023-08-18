2a185f19bb5261b95635838ec8c300e7230557f1
balasathya16 <balasathya16@yahoo.com>
2023-08-18 00:07:59 -0400

    removed the stripe keys from commits

--- backend/controllers/payment_controller.go
+++ backend/controllers/payment_controller.go
@@ -8,11 +8,23 @@ import (
 
 	"github.com/stripe/stripe-go/v72"
 	"github.com/stripe/stripe-go/v72/paymentintent"
-	"github.com/google/uuid" // Add this import
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"os"
 )
+
+// Load .env file to get Stripe secret key
+func loadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+}
 
 // CreatePaymentIntent creates a Stripe Payment Intent for a booking
 func CreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	loadEnv() // Load environment variables from .env
+
 	// Parse the request data, which should include the cricket court UUID and amount
 	var requestData struct {
 		CricketCourtUUID string  `json:"cricketCourtUUID"`
@@ -32,8 +44,11 @@ func CreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
 	}
 	bookingUUID := bookingID.String()
 
+	// Get Stripe secret key from environment variable
+	stripeKey := os.Getenv("STRIPE_SECRET_KEY")
+
 	// Configure your Stripe secret key
-	stripe.Key = "sk_test_51NfSEEG73qJpjALVckj8SuavsjaswtHEIVJxpB42uEzhUsafsRTsFe5Bj35A6R5LL3tEdY812Z1BsH4WW0hNduJH00roVxHGLJ" // Replace with your actual Stripe secret key
+	stripe.Key = stripeKey
 
 	// Create a Stripe Payment Intent
 	intentParams := &stripe.PaymentIntentParams{
@@ -64,27 +79,4 @@ func handlePaymentError(w http.ResponseWriter, statusCode int, message string) {
 	json.NewEncoder(w).Encode(message)
 }
 
-
-// GetPaymentIntent retrieves a Stripe Payment Intent by its ID
-func GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	// Parse the Payment Intent ID from the request parameters
-	intentID := r.URL.Query().Get("intentID")
-	if intentID == "" {
-		handlePaymentError(w, http.StatusBadRequest, "Missing Payment Intent ID")
-		return
-	}
-
-	// Configure your Stripe secret key
-	stripe.Key = "sk_test_51NfSEEG73qJpjALVckj8SuavsjaswtHEIVJxpB42uEzhUsafsRTsFe5Bj35A6R5LL3tEdY812Z1BsH4WW0hNduJH00roVxHGLJ"
-
-	// Retrieve the Payment Intent from Stripe
-	intent, err := paymentintent.Get(intentID, nil)
-	if err != nil {
-		handlePaymentError(w, http.StatusInternalServerError, "Failed to fetch Payment Intent")
-		return
-	}
-
-	// Return the Payment Intent details to frontend
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(intent)
-}
+// Rest of the code for GetPaymentIntent remains the same
