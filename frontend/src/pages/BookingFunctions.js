daa975a809af665fbe4cfca24933da41f61269f3
balasathya16 <balasathya16@yahoo.com>
2023-08-19 00:31:35 -0400

    payment intent from frontend to backend

--- frontend/src/pages/BookingFunctions.js
+++ frontend/src/pages/BookingFunctions.js
@@ -1,31 +1,67 @@
-// bookingFunctions.js
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51NfSEEG73qJpjALVaHz7O59835GKo33MWLrLrmfbtYJVKVeQf6ZE0UZzQCepDteHikbE4rGdxiM8LmvVocJmIAdG00L1DjQ8ex');
 
 const handleConfirmBooking = async (bookingUUID, cricketCourtUUID, amount) => {
     try {
-      const response = await fetch('/api/create-payment-intent', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          bookingUUID,
-          cricketCourtUUID,
-          amount,
-        }),
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        // Call a function to handle successful booking confirmation, e.g., display a success message
-        console.log('Booking confirmed:', data);
-      } else {
-        // Handle error cases here, e.g., show an error message
-        console.error('Failed to confirm booking');
-      }
+        // Step 1: Create a payment intent on the server
+        const paymentResponse = await fetch('http://localhost:8000/api/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                cricketCourtUUID,
+                amount,
+            }),
+        });
+
+        if (!paymentResponse.ok) {
+            console.error('Failed to create payment intent');
+            return;
+        }
+
+        const paymentData = await paymentResponse.json();
+
+        // Step 2: Collect payment details and complete the payment using a payment gateway
+        const stripe = await stripePromise; // Await the stripePromise
+        const elements = stripe.elements();
+
+        const { clientSecret } = paymentData;
+
+        const { error, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.create('card'), // Create a CardElement instance
+            },
+        });
+
+        if (error) {
+            console.error('Payment failed:', error);
+            return;
+        }
+
+        if (paymentIntent.status === 'succeeded') {
+            // Step 3: If payment is successful, send booking confirmation to the server
+            const bookingResponse = await fetch('/api/confirm-booking', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    bookingUUID,
+                }),
+            });
+
+            if (bookingResponse.ok) {
+                console.log('Booking confirmed');
+                // Call a function to handle successful booking confirmation, e.g., display a success message
+            } else {
+                console.error('Failed to confirm booking');
+            }
+        }
     } catch (error) {
-      console.error('An error occurred:', error);
+        console.error('An error occurred:', error);
     }
-  };
-  
-  export default handleConfirmBooking;
-  
+};
+
+export default handleConfirmBooking;
