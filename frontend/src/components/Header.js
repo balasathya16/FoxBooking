fe5fb1767aeaa45ef8b346c9508a6ce6cbd15035
balasathya16 <balasathya16@yahoo.com>
2023-08-27 16:51:32 -0400

    removed sign in button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,9 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import sportsLogo from '../sports.png';
-import AuthContext from '../auth'; // Import the AuthContext
-import GetStartedButton from '../components/HomePage/GetStartedButton'; // Import the GetStartedButton component
 
 const Header = () => {
-  const user = useContext(AuthContext);
 
   return (
     <header className="header">
@@ -20,16 +17,6 @@ const Header = () => {
           <button>
             <i className="fas fa-search"></i>
           </button>
-        </div>
-        <div className="cta-buttons">
-          {user ? (
-            <Link to="/dashboard" className="user-icon">
-              {/* Display user icon or username */}
-              <i className="fas fa-user"></i>
-            </Link>
-          ) : (
-            <GetStartedButton onClick={() => console.log("Open sign up popup")} />
-          )}
         </div>
       </div>
     </header>
