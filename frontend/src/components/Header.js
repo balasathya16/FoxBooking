24cea2e553da80cea1e9fb662498f37263cb103e
balasathya16 <balasathya16@yahoo.com>
2023-09-06 17:05:55 -0400

    header styles changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,33 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-import '../styles/Header.css';
+import AuthContext from '../auth';
+import AuthModal from './AuthModal';
+import '../styles/Header.css'; // Import your Header.css stylesheet
 import sportsLogo from '../sports.png';
-import AuthContext from '../auth'; // Import the AuthContext
 
 const Header = () => {
   const user = useContext(AuthContext);
+  const [isAuthModalOpen, setAuthModalOpen] = useState(false);
+
+  const toggleAuthModal = () => {
+    setAuthModalOpen(!isAuthModalOpen);
+  };
+
+  const handleSignIn = () => {
+    // Add logic for handling sign-in action
+    console.log('Sign In clicked');
+  };
+
+  const handleSignUp = () => {
+    // Add logic for handling sign-up action
+    console.log('Sign Up clicked');
+  };
 
   return (
     <header className="header">
-      <Link to="/" className="logo">
-        <img src={sportsLogo} alt="FoxBooking Logo" />
-        <span className="logo-text">FoxBooking</span>
-      </Link>
-      <div className="navigation">
-        <div className="search-bar">
-          <input type="text" placeholder="Search..." />
-          <button>
-            <i className="fas fa-search"></i>
-          </button>
-          {user ? (
-            <Link to="/dashboard" className="user-icon">
-              <FontAwesomeIcon icon={faUser} className="icon" />
-            </Link>
-          ) : null}
+      <div className="container">
+        <Link to="/" className="logo">
+          <img src={sportsLogo} alt="FoxBooking Logo" />
+          <span className="logo-text">FoxBooking</span>
+        </Link>
+        <div className="navigation">
+          <div className="search-bar">
+            <input type="text" placeholder="Search..." />
+            <button>
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+          <div className="cta-buttons">
+            {user ? (
+              <Link to="/dashboard" className="user-icon">
+                <FontAwesomeIcon icon={faUser} className="icon" />
+              </Link>
+            ) : (
+              <div>
+                <button className="auth-button" onClick={handleSignIn}>
+                  Sign In
+                </button>
+                <button className="auth-button" onClick={handleSignUp}>
+                  Sign Up
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
+      <AuthModal isOpen={isAuthModalOpen} onClose={toggleAuthModal} />
     </header>
   );
 };
