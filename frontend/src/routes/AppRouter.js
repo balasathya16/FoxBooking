defde2e4c229b31c372e5c049b649aa165719a1e
balasathya16 <balasathya16@yahoo.com>
2023-09-17 22:30:01 -0400

    added dashboard route

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -5,18 +5,18 @@ import ListingDetailsPage from '../pages/ListingDetailsPage';
 import PrivacyPolicy from '../pages/PrivacyPolicy';
 import Terms from '../pages/Terms';
 import BookingSummary from '../pages/BookingSummary';
-// Remove the import for SignUpPage
+import Dashboard from '../components/Dashboard'; // Import the Dashboard component
 
 const AppRouter = () => {
   return (
     <Routes>
-      {/* Define your routes here */}
       <Route path="/" element={<HomePage />} />
       <Route path="/listing/:id" element={<ListingDetailsPage />} />
       <Route path="/privacy" element={<PrivacyPolicy />} />
       <Route path="/booking-summary" element={<BookingSummary />} />
       <Route path="/terms" element={<Terms />} />
-      {/* Remove the route for SignUpPage */}
+      {/* Add the route for the Dashboard */}
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
