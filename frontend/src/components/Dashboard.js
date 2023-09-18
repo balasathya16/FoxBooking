58b07f325eed73617d5770381229214277b511e8
balasathya16 <balasathya16@yahoo.com>
2023-09-18 15:35:24 -0400

    user name display

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,11 +3,11 @@ import AuthContext from '../auth'; // Import the AuthContext
 import '../styles/Dashboard.css'; // Create this CSS file
 
 const Dashboard = () => {
-  const user = useContext(AuthContext);
+  const { user } = useContext(AuthContext); // Destructure user from AuthContext
 
   return (
     <div className="dashboard">
-      <h2>Welcome, {user.displayName || 'User'}!</h2>
+      <h2>Welcome, {user ? user.displayName || 'User' : 'User'}!</h2>
       {/* Add more dashboard content here */}
     </div>
   );
