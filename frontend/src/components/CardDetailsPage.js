af719238bdef40bb5482e58bd68a63e4b4ce221a
balasathya16 <balasathya16@yahoo.com>
2023-09-26 00:13:22 -0400

    added files for card details

---
+++ frontend/src/components/CardDetailsPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import MyCheckoutForm from '../pages/PaymentForm.js';
+
+const stripePromise = loadStripe('pk_test_51NfSEEG73qJpjALVaHz7O59835GKo33MWLrLrmfbtYJVKVeQf6ZE0UZzQCepDteHikbE4rGdxiM8LmvVocJmIAdG00L1DjQ8ex'); // Replace with your actual public key
+
+const CardDetailsPage = () => {
+  return (
+    <div>
+      <h1>Enter Card Details</h1>
+      <Elements stripe={stripePromise}>
+        <MyCheckoutForm />
+      </Elements>
+    </div>
+  );
+};
+
+export default CardDetailsPage;
---
+++ frontend/src/pages/PaymentForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const MyCheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+    if (error) {
+      console.error('Error creating payment method:', error);
+    } else {
+      // Use paymentMethod.id to complete the payment on your server
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+export default MyCheckoutForm;
