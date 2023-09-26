717b127ab4c262241bec778b87daed5aef36bb9f
balasathya16 <balasathya16@yahoo.com>
2023-09-26 00:10:07 -0400

    added card details

--- frontend/src/pages/BookingSummary.js
+++ frontend/src/pages/BookingSummary.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { loadStripe } from '@stripe/stripe-js'; // Import loadStripe
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import MyCheckoutForm from './PaymentForm'; // Import the PaymentForm
 import '../styles/BookingSummary.css';
-import handleConfirmBooking from './BookingFunctions.js'; // Import the handleConfirmBooking function
+import handleConfirmBooking from './BookingFunctions.js';
+
+const stripePromise = loadStripe('pk_test_51NfSEEG73qJpjALVaHz7O59835GKo33MWLrLrmfbtYJVKVeQf6ZE0UZzQCepDteHikbE4rGdxiM8LmvVocJmIAdG00L1DjQ8ex');
 
 const BookingSummary = () => {
-  const stripePromise = loadStripe('pk_test_51NfSEEG73qJpjALVaHz7O59835GKo33MWLrLrmfbtYJVKVeQf6ZE0UZzQCepDteHikbE4rGdxiM8LmvVocJmIAdG00L1DjQ8ex'); // Load Stripe instance
   const location = useLocation();
+  const navigate = useNavigate(); // Add useNavigate
   const { selectedDate, startTime, endTime, pricePerHour, listing } = location.state;
 
   const startTimeObj = new Date(startTime);
@@ -22,25 +26,27 @@ const BookingSummary = () => {
     amount: totalCost + taxes,
   });
 
-  // Custom options for formatting time without seconds
   const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
 
   const handleConfirmBookingClick = async () => {
     try {
-      const stripe = await stripePromise; // Wait for stripePromise
+      const stripe = await stripePromise;
       await handleConfirmBooking(
         bookingData.bookingUUID,
         bookingData.cricketCourtUUID,
         bookingData.amount,
-        stripe // Pass the stripe instance
+        stripe
       );
+
+      // Redirect to the card details page after confirming the booking
+      navigate('/card-details'); // Update the path to your card details page
     } catch (error) {
       console.error('Error confirming booking:', error);
     }
   };
 
   return (
-       <div className="booking-summary-container">
+    <div className="booking-summary-container">
       <h1>Booking Summary</h1>
       <div className="booking-summary-listing-info">
         {listing.images && listing.images.length > 0 ? (
@@ -72,6 +78,9 @@ const BookingSummary = () => {
         <p>Total:</p>
         <p>${totalCost.toFixed(2)}</p>
       </div>
+      <Elements stripe={stripePromise}>
+        <MyCheckoutForm />
+      </Elements>
       <button className="booking-summary-confirm-button" onClick={handleConfirmBookingClick}>
         Confirm Booking
       </button>
