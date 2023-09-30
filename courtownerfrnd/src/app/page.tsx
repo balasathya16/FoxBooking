55922723d1af59f60b15516f7e9a1d3b8227b088
balasathya16 <balasathya16@yahoo.com>
2023-09-30 12:02:16 -0400

    added link to create court

--- courtownerfrnd/src/app/page.tsx
+++ courtownerfrnd/src/app/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 const SamplePage = () => {
   return (
     <div>
       <h1>Sample Page</h1>
       <p>This is a sample page content.</p>
+
+      <Link href="/create-court">
+        Go to Create Court
+      </Link>
     </div>
   );
 };
