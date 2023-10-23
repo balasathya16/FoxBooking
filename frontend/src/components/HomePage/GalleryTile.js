f924ef4612f956a81eaaefc4371e72a172593490
balasathya16 <balasathya16@yahoo.com>
2023-10-23 00:07:36 -0400

    added logging to diagnose gallerytile listing not fetching data from header's search results

--- frontend/src/components/HomePage/GalleryTile.js
+++ frontend/src/components/HomePage/GalleryTile.js
@@ -1,25 +1,33 @@
-// GalleryTile.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ListingTile from './ListingTile';
 import '../../styles/GalleryTile.css';
 
-
-
-const GalleryTile = () => {
+const GalleryTile = ({ searchResults }) => {
+  console.log('GalleryTile received searchResults:', searchResults);
   const [listings, setListings] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    console.log('useEffect in GalleryTile triggered');
+    console.log('Search Results:', searchResults);
+    
     const fetchListings = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/cricket');
-        const { data } = response;
-
-        if (Array.isArray(data) && data.length > 0) {
-          setListings(data);
+        if (searchResults && searchResults.length > 0) {
+          // If search results are available, use them
+          console.log('Using search results:', searchResults);
+          setListings(searchResults);
         } else {
-          console.error('Invalid or empty API response:', data);
+          const response = await axios.get('http://127.0.0.1:8000/cricket');
+          const { data } = response;
+
+          if (Array.isArray(data) && data.length > 0) {
+            console.log('Using API results:', data);
+            setListings(data);
+          } else {
+            console.error('Invalid or empty API response:', data);
+          }
         }
       } catch (error) {
         console.error('Error fetching listings:', error);
@@ -29,7 +37,7 @@ const GalleryTile = () => {
     };
 
     fetchListings();
-  }, []);
+  }, [searchResults]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -42,6 +50,7 @@ const GalleryTile = () => {
   return (
     <div className="gallery-tile">
       <div className="listing-container">
+      {console.log('Listings:', listings)}
         {listings.map((listing) => (
           <ListingTile key={listing.id} listing={listing} />
         ))}
