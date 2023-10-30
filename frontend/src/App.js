66aba9b1a5fff2ec2c4813e67e93523c89ed5674
balasathya16 <balasathya16@yahoo.com>
2023-10-30 15:41:31 -0400

    moving search logic in app so other pages reflect search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import Header from './components/Header';
+import React, { useState } from 'react';
+import Header from '../src/components/Header';
 import Footer from './components/Footer';
+import AppRouter from './routes/AppRouter';
 import './styles/Header.css';
 import './styles/Footer.css';
 import './styles/tailwind.css';
-import AppRouter from './routes/AppRouter';
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const updateSearchResults = (results) => {
+    setSearchResults(results);
+  };
+
   return (
     <div className="App">
-       <div className="App-content">
-          <AppRouter />
-       </div>
-       <Footer />
+      <div className="App-content">
+        <Header updateSearchResults={updateSearchResults} />
+        <AppRouter searchResults={searchResults} />
+      </div>
+      <Footer />
     </div>
   );
 };
